2e39500cfd9b39b609a0d716ce8f6406ddcc9d8b
Alfiya Shekh <alfiyashekh521@gmail.com>
2025-06-11 05:54:30 +0000

    datatypes are done

---
+++ basics/05_datatypes.js
@@ -0,0 +1,22 @@
+//number
+let age=18
+console.log(age)
+
+//string
+let name="alfiya"
+console.log(name);
+
+//boolean
+let loggedIn=true
+console.log(loggedIn)
+
+//null
+let status=null
+console.log(status);
+
+//undefined
+let position
+console.log(position);
+
+
+
