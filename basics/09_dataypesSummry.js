f78f843de243431d035434e070ba879a216cf438
Alfiya Shekh <alfiyashekh521@gmail.com>
2025-06-11 15:30:39 +0000

    Datatypes Done

---
+++ basics/09_dataypesSummry.js
@@ -0,0 +1,27 @@
+// ## Primitive types
+
+let a=10.2 // NUmber
+let b="abc" //string
+let c=true // boolean
+let d=null //empty value
+let e //undefined value
+const id=Symbol('123')  //symbol
+const big=12345678n //BigInt
+ //BigInt,symbol ae well
+
+
+
+ // ## Non-primitive / reference 
+  let arr=Array(2,3) // array
+
+  function fun(){
+    console.log("This is function");
+    
+  }   // function
+
+  let obj={
+    name:"alfiya",
+    age:18
+  }  //object
+
+ 
