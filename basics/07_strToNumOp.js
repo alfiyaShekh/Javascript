0552b583d899db215e3581a85e92c9ca0ba995a9
Alfiya Shekh <alfiyashekh521@gmail.com>
2025-06-11 06:55:54 +0000

    conversion operations

---
+++ basics/07_strToNumOp.js
@@ -0,0 +1,54 @@
+let value=3
+let neg= -value
+console.log(neg);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**2);
+
+let str="alfiya"
+let str2=" shekh"
+console.log(str+str2); // simple string concatination
+
+console.log(1+"2");
+console.log("1"+2);
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"1");
+
+console.log((+true));
+console.log(+"");
+
+//increatment & decrement
+a=10
+c=a++ //post increment
+console.log(c); 
+b=++a //pre increment
+console.log(b);
+
+
+b=10
+c=b--
+console.log(c); //post decrement
+d=--b
+console.log(b); //pre decrement 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
