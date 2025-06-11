b64570c8d6df6fbb6e21ccd24892d8b20177ff4a
Alfiya Shekh <alfiyashekh521@gmail.com>
2025-06-11 07:09:44 +0000

    Dataypes Comparison

---
+++ basics/08_comparisonDatatypes.js
@@ -0,0 +1,23 @@
+console.log("2" > 1);
+console.log(("02" >1));
+
+console.log(null >0);
+console.log(null ==0);
+console.log(null >= 0);
+// comparison convert null into 0 therefor null>=0 is true and null>0 is  false
+
+console.log(undefined >0 );
+console.log(undefined == 0 );
+console.log(undefined >= 0 );
+
+
+// === =>check equality as well as datatype
+console.log(2===2);
+console.log("2" === 2);
+
+console.log(true >= 1); // while comparing true will converted to 1
+console.log(false >1); // while comparing true will converted to 0
+console.log(true =="abc");
+
+
+
