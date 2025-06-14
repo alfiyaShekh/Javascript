d41a79e1b29140991b5ed06c1a9b97c548d6122b
Alfiya Shekh <alfiyashekh521@gmail.com>
2025-06-14 10:09:15 +0000

    Object Destructuring

---
+++ objects/03_object.js
@@ -0,0 +1,14 @@
+// destructuring of objects 
+const course={
+    courseName:"js",
+    price:0,
+    course_teacher:"hitesh sir"
+}
+
+const {price}=course 
+console.log(price); // course.price can be avoided and directly we can use price key
+
+const {course_teacher:teacher}=course  // we can provide another name to key as well
+console.log(teacher);
+
+
