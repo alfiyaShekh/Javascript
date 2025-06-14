dc6fef2774114eb4928edc9c60b6e482fae336d8
Alfiya Shekh <alfiyashekh521@gmail.com>
2025-06-14 17:28:01 +0000

    scope with mini hosting

---
+++ 22_scope.js
@@ -0,0 +1,43 @@
+// nested scope
+
+function one(){
+    const user="alfiya"
+    function two(){
+        const lastname="shekh"
+        console.log(user);
+        console.log(lastname); // can access parent function property
+        
+    }
+    //console.log(lastname); //can't access child function property
+    two()
+}
+//one()
+//two() // can't access child function outside of parent function
+
+
+
+if(true){
+    const username="alfiya"
+    if(username==="alfiya"){
+        const sirname="shekh"
+        console.log(username+" "+sirname);
+        
+    }
+    //console.log(sirname); // not accessible coz of blk scope
+    
+}
+//console.log(username); // not accessible coz it's scope is within that blk
+
+
+console.log(addone(5));
+function addone(num){
+    return num+1
+}
+
+
+
+addtwo(5)
+// variable function will give error if called before its function definition
+ const addtwo=function(num){
+    console.log(num+2);
+}
