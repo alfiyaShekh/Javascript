f5d4170fbc20b9389781471fd8c99434e2510437
Alfiya Shekh <alfiyashekh521@gmail.com>
2025-06-16 13:12:34 +0000

    while, do-while

---
+++ 28_whileDoWhile.js
@@ -0,0 +1,35 @@
+// ** while loop **
+let i=1
+while(i<=10){
+    //console.log(i);
+    i=i+2
+    
+}
+
+// ** while with array **
+let arr=[1,2,3,4]
+let j=0
+while(j<arr.length){
+    //console.log(arr[j]);
+    j++
+    
+}
+
+
+// ** do-while **
+let k=1
+do{
+    console.log(k)
+    k++;
+    
+}while(k<=0);
+
+
+// ** array with do-while  **
+let array=[5,6,7,8]
+let x=0
+do{
+    console.log(array[x]);
+    x++
+    
+}while(x<array.length)
