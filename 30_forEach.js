ab33ce72004b6f5ef74cafa0405cc10ddd8a5711
Alfiya Shekh <alfiyashekh521@gmail.com>
2025-06-16 18:04:12 +0000

    for each loop

---
+++ 30_forEach.js
@@ -0,0 +1,15 @@
+// ** for Each Loop **
+
+const coding=["js","java","c++","php"]
+coding.forEach(element => {
+    console.log(element);
+    
+});
+
+
+
+function printme(item){
+   console.log(item);
+   
+}
+coding.forEach(printme)
