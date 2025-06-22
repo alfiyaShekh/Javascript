b4ce3f297b32accac98f600ac827e08ae3774f9f
Alfiya Shekh <alfiyashekh521@gmail.com>
2025-06-22 18:43:29 +0530

    Project1(color changer)

--- Projects/project1/project1.js
+++ Projects/project1/project1.js
@@ -3,10 +3,28 @@ const body=document.querySelector('body')
 
 console.log(btn);
 
+// btn.forEach(function(button){
+//     console.log(button);
+//     button.addEventListener('click',function(e){
+       
+//         e.preventDefault()
+//         body.style.backgroundColor=e.target.id
+//     })
+// })
+
+
 btn.forEach(function(button){
-    console.log(button);
     button.addEventListener('click',function(e){
-        e.preventDefault()
-        body.style.backgroundColor=e.target.id
+        const color=e.target.id
+        switch(color){
+            case 'red':
+            case 'yellow':
+            case "blue":
+            case 'orange':
+                body.style.backgroundColor=color
+                break;
+            default:alert("Enter on boxes to change color")
+            break;
+        }
     })
 })
